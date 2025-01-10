7dde4c47a87225a2cc3864d4271775e29d7791e6
YingLian Chen <78669829+YLCHEN1992@users.noreply.github.com>
2025-01-10 14:44:37 +0800

    Add files via upload

---
+++ CMDnohup.sh
@@ -0,0 +1,16 @@
+m=$(cat ./cmds|wc -l)
+n=1
+while true
+do 
+running=$(ps -ef|grep ylchen |grep /usr/lib/R/bin/exec/R |grep -v grep|wc -l)
+if [ 8 -gt $running ];then
+eval $(cat ./cmds |head -n ${n}|tail -n 1)
+n=`expr ${n} + 1`
+sleep 3
+else
+sleep 10
+fi
+if [ $n -gt $m ];then
+break
+fi
+done
