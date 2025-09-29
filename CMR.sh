bfb6063c4068b864b5b9f82b4ed25b7a50c85d36
YingLian Chen <78669829+YLCHEN1992@users.noreply.github.com>
2025-09-29 11:34:56 +0800

    Update CMR.sh

--- CMR.sh
+++ CMR.sh
@@ -1,6 +1,6 @@
 Rscript - $1 $2 <<EOR
 args=commandArgs(trailingOnly = TRUE)
 bbj1=args[1];bbj2=args[2]
-source('ftp://systempackage.cn/R/CMR.r')
+source('./CMR.r')
 CMR(bbj1,bbj2,saveload='../MRresults')
 EOR
