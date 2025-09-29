42b23899d63166ab46166757630c5a5edec63614
YingLian Chen <78669829+YLCHEN1992@users.noreply.github.com>
2025-09-29 11:34:15 +0800

    Update CMDnohup.sh

--- CMDnohup.sh
+++ CMDnohup.sh
@@ -2,8 +2,8 @@ m=$(cat ./cmds|wc -l)
 n=1
 while true
 do 
-running=$(ps -ef|grep ylchen |grep /usr/lib/R/bin/exec/R |grep -v grep|wc -l)
-if [ 8 -gt $running ];then
+running=$(ps -ef|grep /usr/lib/R/bin/exec/R |grep -v grep|wc -l)
+if [ $1 -gt $running ];then
 eval $(cat ./cmds |head -n ${n}|tail -n 1)
 n=`expr ${n} + 1`
 sleep 3
